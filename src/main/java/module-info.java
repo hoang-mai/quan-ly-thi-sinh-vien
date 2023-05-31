49ea21aa1c2b379bb758d0ed4edecbdc71cfb4f8
hoang-mai <maianhhoang31072003@gmail.com>
2023-05-31 16:06:00 +0700

    init project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.example.project {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.hibernate.orm.core;
+    requires java.persistence;
+
 
     opens com.example.project to javafx.fxml;
     exports com.example.project;
