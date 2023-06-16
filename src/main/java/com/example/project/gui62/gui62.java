b9d10adb77cdac90d90c0b3979312023915f28b6
hoang-mai <maianhhoang31072003@gmail.com>
2023-06-16 08:19:30 +0700

    init project

---
+++ src/main/java/com/example/project/gui62/gui62.java
@@ -0,0 +1,18 @@
+package com.example.project.gui62;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class gui62 {
+
+    @FXML
+    void anewquestion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void arandomquestion(ActionEvent event) {
+
+    }
+
+}
